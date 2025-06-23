f83a73e57347216331943be7d4e2388c6a6aef37
wegweesgwesg <wegweesgwesg@gmail.com>
2025-06-23 12:14:33 +0300

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,7 @@ const slides = [
   {
     img: "images/slider1.jpg",
     title: "Собираем компы",
-    text: "О да мы собираем компы твоей мечты, ты таких во сне даже не увидишь никогда..."
+    text: "Да мы собираем компы вашей мечты"
   },
   {
     img: "images/slider1.jpg",
@@ -27,10 +27,10 @@ let startX = 0;
 let isDragging = false;
 let autoTimer = null;
 
-// 1. Генерируем слайды и точки
+
 function initSlider() {
   slides.forEach((slide, i) => {
-    // слайд
+
     const s = document.createElement("div");
     s.classList.add("main_slide");
     s.style.background = 
@@ -42,7 +42,7 @@ function initSlider() {
     `;
     inner.appendChild(s);
 
-    // точка
+
     const dot = document.createElement("div");
     dot.classList.add("dot");
     dot.addEventListener("click", () => goTo(i));
@@ -52,7 +52,7 @@ function initSlider() {
   startAuto();
 }
 
-// 2. Переключение
+
 function update() {
   inner.style.transform = `translateX(-${current * 100}%)`;
   dotsContainer.querySelectorAll(".dot").forEach((dot, i) =>
@@ -60,14 +60,14 @@ function update() {
   );
 }
 
-// 3. Перейти на слайд i
+
 function goTo(i) {
   current = i;
   resetAuto();
   update();
 }
 
-// 4. Перетаскивание
+
 slider.addEventListener("mousedown", e => {
   isDragging = true;
   startX = e.clientX;
@@ -77,7 +77,7 @@ slider.addEventListener("mousedown", e => {
 window.addEventListener("mousemove", e => {
   if (!isDragging) return;
   const diff = e.clientX - startX;
-  // если перетащили вправо/влево на 50px
+
   if (diff > 50 && current > 0) {
     goTo(current - 1);
     isDragging = false;
@@ -92,7 +92,7 @@ window.addEventListener("mouseup", () => {
   startAuto();
 });
 
-// 5. Автопрокрутка
+
 function startAuto() {
   autoTimer = setInterval(() => {
     current = (current + 1) % slides.length;
